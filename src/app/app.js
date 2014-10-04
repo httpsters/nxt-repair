ce97c7df142a18d6a26054f65b390b6900c334ff
Michael Woloszuk <mwoloszuk@gmail.com>
2014-10-04 16:46:30 -0400

    Ready files for development

---
+++ src/app/app.js
@@ -0,0 +1,52 @@
+angular.module( 'repair', [
+	'repairControllers',
+  'ngRoute'
+])
+
+.config([ '$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $routeProvider
+      // Start Page
+      .when('/', {
+        templateUrl: 'app/partials/start.html',
+        controller: 'StartController'
+      })
+      // Garage Page
+      .when('/garage', {
+        templateUrl: 'app/partials/garage.html',
+        controller: 'GarageController',
+      })
+      // Mechanics Page
+      .when('/mechanics', {
+        templateUrl: 'app/partials/mechanics.html',
+        controller: 'MechanicsController',
+      })
+      // Listings Page
+      .when('/listings', {
+        templateUrl: 'app/partials/listings.html',
+        controller: 'ListingsController',
+      })
+      // Whatever Page
+      .when('/whatever', {
+        templateUrl: 'app/partials/whatever.html',
+        controller: 'WhateverController',
+      })
+      // Add Extra Pages Here
+      // .when('/name', {
+      //   templateUrl: 'app/partials/name.html',
+      //   controller: 'NameController',
+      // })
+      .otherwise({redirectTo:'/'});
+      $locationProvider.html5Mode(true);
+}])
+
+.run(function run () {
+
+})
+
+.controller('AppCtrl', [ '$scope', '$location', function ($scope, $location) {
+  $scope.linkTo = function (location) {
+		$location.path('/' + location);
+	};
+}])
+
+;
---
+++ src/app/controllers.js
@@ -0,0 +1,23 @@
+angular.module('repairControllers', [])
+
+.controller('StartController', [ '$scope', '$http', '$window', '$sce', '$location', function ($scope, $http, $window, $sce, $location) {
+
+}])
+
+.controller('GarageController', [ '$scope', '$http', '$window', '$sce', '$location', function ($scope, $http, $window, $sce, $location) {
+
+}])
+
+.controller('MechanicsController', [ '$scope', '$http', '$window', '$sce', '$location', function ($scope, $http, $window, $sce, $location) {
+
+}])
+
+.controller('ListingsController', [ '$scope', '$http', '$window', '$sce', '$location', function ($scope, $http, $window, $sce, $location) {
+
+}])
+
+.controller('WhateverController', [ '$scope', '$http', '$window', '$sce', '$location', function ($scope, $http, $window, $sce, $location) {
+
+}])
+
+;
